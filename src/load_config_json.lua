3955364b5432b4d6b2780ffcaa9579533b07b334
yangqiong <yangqiong@corp.netease.com>
2017-01-13 16:34:23 +0800

    update file

--- src/load_config_json.lua
+++ src/load_config_json.lua
@@ -6,7 +6,7 @@ local dogs = ngx.shared.dogs
 local _M = {}
 
 --load config
-function _M.load_config(file_name)
+function _M.load_config_json(file_name)
   local result = dogs:get(file_name)
   if  result then
     return result
@@ -19,21 +19,21 @@ function _M.load_config(file_name)
 end
 --get distribute
 function _M.get_distribute(file_name)
-  local result = _M.load_config(file_name)
+  local result = _M.load_config_json(file_name)
   local jsontable = json.decode(result)
   local distribute = jsontable['distribute']
   return distribute
 end
 --get need delete_key
 function _M.get_need_delete_key(file_name)
-  local result = _M.load_config(file_name)
+  local result = _M.load_config_json(file_name)
   local jsontable = json.decode(result)
   local delete_key = jsontable['delete_key']
   return delete_key
 end
 --get relational_mapping
 function _M.get_relational_mapping(file_name)
-  local result = _M.load_config(file_name)
+  local result = _M.load_config_json(file_name)
   local jsontable = json.decode(result)
   local relational_mapping = jsontable['relational_mapping']
   return relational_mapping
